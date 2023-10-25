7a3d3bc6502925ceccdd9cd5960b25884461b285
wsj7178 <wsj7178@naver.com>
2023-10-25 20:02:06 +0900

    add some puppeteer method

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,42 @@
-import puppeteer from 'puppeteer'
+import 'dotenv/config'
+import puppeteer, { ElementHandle, Page } from 'puppeteer'
+
+function clickLecture(page: Page, lectureId: string) {
+  return page.click(`a[onclick="fn_goLecture('${lectureId}')"]`)
+}
+
+async function clickStudy(page: Page, week: number) {
+  const trEl = await page.waitForSelector(`tr[class*="${week}_ws1"]`, {
+    timeout: 5000,
+  })
+  if (!trEl) throw new Error('No element')
+  ;(trEl as ElementHandle<Element>).click()
+}
 
 async function main() {
+  const lectureId = process.env.lecture_num!
+
   const browser = await puppeteer.launch({
     headless: false,
   })
   const page = await browser.newPage()
-
+  await page.setCookie({
+    name: 'tongken',
+    value: '8d0d202d7c9f4c7f9fb1756d5c549086',
+    domain: '.iscu.ac.kr',
+    path: '/',
+  })
+  await page.setViewport({
+    width: 1280,
+    height: 720,
+  })
   await page.goto('https://home.iscu.ac.kr/wlms/lechall/main/mainList.scu')
-  // Set screen size
-  await page.setViewport({ width: 1080, height: 1024 })
+  await clickLecture(page, lectureId)
+  await clickStudy(page, 3)
 }
 
-main()
+try {
+  main()
+} catch (e) {
+  console.error(e)
+}
