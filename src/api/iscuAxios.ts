c108f0378b5fabe215831945eaa91476164b264c
wsj7178 <wsj7178@naver.com>
2023-10-26 11:42:01 +0900

    add semi

--- src/api/iscuAxios.ts
+++ src/api/iscuAxios.ts
@@ -2,6 +2,6 @@ import axios from 'axios'
 
 export const iscuAxios = axios.create({
   headers: {
-    Cookie: `tongken=${process.env.token}`,
+    Cookie: `tongken=${process.env.token};`,
   },
 })
